f09e73ce026dc86552f813637277a8c6180c6bc7
zuwiki <zuwiki@gmail.com>
2008-09-30 16:37:46 -0700

    Add form for creating new posts.

--- models.rb
+++ models.rb
@@ -5,10 +5,9 @@ class Post
   use_database CouchRest.database!("http://localhost:5984/seclusion-test")
 
   view_by :created_at, :descending => true
-  view_by :user_id, :date
 
   key_reader :created_at, :updated_at
-  key_accessor :body, :tags
+  key_accessor :body
   
   timestamps!
 end
--- seclusion.rb
+++ seclusion.rb
@@ -13,13 +13,30 @@ load "helpers.rb"
 load "models.rb"
 
 ### Actions
+
+# Lists all posts sorted by date
 get '/' do
   title "index"
   @posts = Post.by_created_at
   haml :index
 end
 
-get '/:id' do
+# Page for creating new posts
+get '/post' do
+  haml :new_post
+end
+
+# Endpoint for creating posts
+post '/post' do
+  @post = Post.new
+  @post.body = params[:body]
+  # TODO: Validate first, or include validation by hacking up CouchRest
+  @post.save
+  redirect "/post/" + @post.id
+end
+
+# Individual views for posts
+get '/post/:id' do
   @post = Post.get(params[:id])
   haml :post
 end
